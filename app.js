8e42a97c4edccad21a81209c2804ed1b576ed7dc
Denis <denis.o.shapiro@gmail.com>
2020-09-03 13:47:47 -0400

    bug fix with dropdown

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var compression = require('compression');
-var helmet = require('helmet');
 
 var app = express();
 
@@ -21,14 +19,13 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 // view engine setup
-app.use(helmet());
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(compression()); //Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
